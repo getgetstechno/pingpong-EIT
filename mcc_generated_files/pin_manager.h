1404f300413a23b11aed28edcb2de3dfd11fa46b
getgetstechno <66800705+getgetstechno@users.noreply.github.com>
2024-03-05 20:18:16 +0100

    added mcc_generated_files

---
+++ mcc_generated_files/pin_manager.h
@@ -0,0 +1,269 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  @Description
+    This header file provides APIs for driver for .
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.7
+        Device            :  PIC16F18345
+        Driver Version    :  2.11
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 2.31 and above
+        MPLAB 	          :  MPLAB X 5.45	
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+/**
+  Section: Included Files
+*/
+
+#include <xc.h>
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set RA0 procedures
+#define RA0_SetHigh()            do { LATAbits.LATA0 = 1; } while(0)
+#define RA0_SetLow()             do { LATAbits.LATA0 = 0; } while(0)
+#define RA0_Toggle()             do { LATAbits.LATA0 = ~LATAbits.LATA0; } while(0)
+#define RA0_GetValue()              PORTAbits.RA0
+#define RA0_SetDigitalInput()    do { TRISAbits.TRISA0 = 1; } while(0)
+#define RA0_SetDigitalOutput()   do { TRISAbits.TRISA0 = 0; } while(0)
+#define RA0_SetPullup()             do { WPUAbits.WPUA0 = 1; } while(0)
+#define RA0_ResetPullup()           do { WPUAbits.WPUA0 = 0; } while(0)
+#define RA0_SetAnalogMode()         do { ANSELAbits.ANSA0 = 1; } while(0)
+#define RA0_SetDigitalMode()        do { ANSELAbits.ANSA0 = 0; } while(0)
+
+// get/set LED aliases
+#define LED_TRIS                 TRISAbits.TRISA5
+#define LED_LAT                  LATAbits.LATA5
+#define LED_PORT                 PORTAbits.RA5
+#define LED_WPU                  WPUAbits.WPUA5
+#define LED_OD                   ODCONAbits.ODCA5
+#define LED_ANS                  ANSELAbits.ANSA5
+#define LED_SetHigh()            do { LATAbits.LATA5 = 1; } while(0)
+#define LED_SetLow()             do { LATAbits.LATA5 = 0; } while(0)
+#define LED_Toggle()             do { LATAbits.LATA5 = ~LATAbits.LATA5; } while(0)
+#define LED_GetValue()           PORTAbits.RA5
+#define LED_SetDigitalInput()    do { TRISAbits.TRISA5 = 1; } while(0)
+#define LED_SetDigitalOutput()   do { TRISAbits.TRISA5 = 0; } while(0)
+#define LED_SetPullup()          do { WPUAbits.WPUA5 = 1; } while(0)
+#define LED_ResetPullup()        do { WPUAbits.WPUA5 = 0; } while(0)
+#define LED_SetPushPull()        do { ODCONAbits.ODCA5 = 0; } while(0)
+#define LED_SetOpenDrain()       do { ODCONAbits.ODCA5 = 1; } while(0)
+#define LED_SetAnalogMode()      do { ANSELAbits.ANSA5 = 1; } while(0)
+#define LED_SetDigitalMode()     do { ANSELAbits.ANSA5 = 0; } while(0)
+
+// get/set RB4 procedures
+#define RB4_SetHigh()            do { LATBbits.LATB4 = 1; } while(0)
+#define RB4_SetLow()             do { LATBbits.LATB4 = 0; } while(0)
+#define RB4_Toggle()             do { LATBbits.LATB4 = ~LATBbits.LATB4; } while(0)
+#define RB4_GetValue()              PORTBbits.RB4
+#define RB4_SetDigitalInput()    do { TRISBbits.TRISB4 = 1; } while(0)
+#define RB4_SetDigitalOutput()   do { TRISBbits.TRISB4 = 0; } while(0)
+#define RB4_SetPullup()             do { WPUBbits.WPUB4 = 1; } while(0)
+#define RB4_ResetPullup()           do { WPUBbits.WPUB4 = 0; } while(0)
+#define RB4_SetAnalogMode()         do { ANSELBbits.ANSB4 = 1; } while(0)
+#define RB4_SetDigitalMode()        do { ANSELBbits.ANSB4 = 0; } while(0)
+
+// get/set RB5 procedures
+#define RB5_SetHigh()            do { LATBbits.LATB5 = 1; } while(0)
+#define RB5_SetLow()             do { LATBbits.LATB5 = 0; } while(0)
+#define RB5_Toggle()             do { LATBbits.LATB5 = ~LATBbits.LATB5; } while(0)
+#define RB5_GetValue()              PORTBbits.RB5
+#define RB5_SetDigitalInput()    do { TRISBbits.TRISB5 = 1; } while(0)
+#define RB5_SetDigitalOutput()   do { TRISBbits.TRISB5 = 0; } while(0)
+#define RB5_SetPullup()             do { WPUBbits.WPUB5 = 1; } while(0)
+#define RB5_ResetPullup()           do { WPUBbits.WPUB5 = 0; } while(0)
+#define RB5_SetAnalogMode()         do { ANSELBbits.ANSB5 = 1; } while(0)
+#define RB5_SetDigitalMode()        do { ANSELBbits.ANSB5 = 0; } while(0)
+
+// get/set RB6 procedures
+#define RB6_SetHigh()            do { LATBbits.LATB6 = 1; } while(0)
+#define RB6_SetLow()             do { LATBbits.LATB6 = 0; } while(0)
+#define RB6_Toggle()             do { LATBbits.LATB6 = ~LATBbits.LATB6; } while(0)
+#define RB6_GetValue()              PORTBbits.RB6
+#define RB6_SetDigitalInput()    do { TRISBbits.TRISB6 = 1; } while(0)
+#define RB6_SetDigitalOutput()   do { TRISBbits.TRISB6 = 0; } while(0)
+#define RB6_SetPullup()             do { WPUBbits.WPUB6 = 1; } while(0)
+#define RB6_ResetPullup()           do { WPUBbits.WPUB6 = 0; } while(0)
+#define RB6_SetAnalogMode()         do { ANSELBbits.ANSB6 = 1; } while(0)
+#define RB6_SetDigitalMode()        do { ANSELBbits.ANSB6 = 0; } while(0)
+
+// get/set RB7 procedures
+#define RB7_SetHigh()            do { LATBbits.LATB7 = 1; } while(0)
+#define RB7_SetLow()             do { LATBbits.LATB7 = 0; } while(0)
+#define RB7_Toggle()             do { LATBbits.LATB7 = ~LATBbits.LATB7; } while(0)
+#define RB7_GetValue()              PORTBbits.RB7
+#define RB7_SetDigitalInput()    do { TRISBbits.TRISB7 = 1; } while(0)
+#define RB7_SetDigitalOutput()   do { TRISBbits.TRISB7 = 0; } while(0)
+#define RB7_SetPullup()             do { WPUBbits.WPUB7 = 1; } while(0)
+#define RB7_ResetPullup()           do { WPUBbits.WPUB7 = 0; } while(0)
+#define RB7_SetAnalogMode()         do { ANSELBbits.ANSB7 = 1; } while(0)
+#define RB7_SetDigitalMode()        do { ANSELBbits.ANSB7 = 0; } while(0)
+
+// get/set Potentiometer aliases
+#define Potentiometer_TRIS                 TRISCbits.TRISC0
+#define Potentiometer_LAT                  LATCbits.LATC0
+#define Potentiometer_PORT                 PORTCbits.RC0
+#define Potentiometer_WPU                  WPUCbits.WPUC0
+#define Potentiometer_OD                   ODCONCbits.ODCC0
+#define Potentiometer_ANS                  ANSELCbits.ANSC0
+#define Potentiometer_SetHigh()            do { LATCbits.LATC0 = 1; } while(0)
+#define Potentiometer_SetLow()             do { LATCbits.LATC0 = 0; } while(0)
+#define Potentiometer_Toggle()             do { LATCbits.LATC0 = ~LATCbits.LATC0; } while(0)
+#define Potentiometer_GetValue()           PORTCbits.RC0
+#define Potentiometer_SetDigitalInput()    do { TRISCbits.TRISC0 = 1; } while(0)
+#define Potentiometer_SetDigitalOutput()   do { TRISCbits.TRISC0 = 0; } while(0)
+#define Potentiometer_SetPullup()          do { WPUCbits.WPUC0 = 1; } while(0)
+#define Potentiometer_ResetPullup()        do { WPUCbits.WPUC0 = 0; } while(0)
+#define Potentiometer_SetPushPull()        do { ODCONCbits.ODCC0 = 0; } while(0)
+#define Potentiometer_SetOpenDrain()       do { ODCONCbits.ODCC0 = 1; } while(0)
+#define Potentiometer_SetAnalogMode()      do { ANSELCbits.ANSC0 = 1; } while(0)
+#define Potentiometer_SetDigitalMode()     do { ANSELCbits.ANSC0 = 0; } while(0)
+
+// get/set Hoogtesensor aliases
+#define Hoogtesensor_TRIS                 TRISCbits.TRISC2
+#define Hoogtesensor_LAT                  LATCbits.LATC2
+#define Hoogtesensor_PORT                 PORTCbits.RC2
+#define Hoogtesensor_WPU                  WPUCbits.WPUC2
+#define Hoogtesensor_OD                   ODCONCbits.ODCC2
+#define Hoogtesensor_ANS                  ANSELCbits.ANSC2
+#define Hoogtesensor_SetHigh()            do { LATCbits.LATC2 = 1; } while(0)
+#define Hoogtesensor_SetLow()             do { LATCbits.LATC2 = 0; } while(0)
+#define Hoogtesensor_Toggle()             do { LATCbits.LATC2 = ~LATCbits.LATC2; } while(0)
+#define Hoogtesensor_GetValue()           PORTCbits.RC2
+#define Hoogtesensor_SetDigitalInput()    do { TRISCbits.TRISC2 = 1; } while(0)
+#define Hoogtesensor_SetDigitalOutput()   do { TRISCbits.TRISC2 = 0; } while(0)
+#define Hoogtesensor_SetPullup()          do { WPUCbits.WPUC2 = 1; } while(0)
+#define Hoogtesensor_ResetPullup()        do { WPUCbits.WPUC2 = 0; } while(0)
+#define Hoogtesensor_SetPushPull()        do { ODCONCbits.ODCC2 = 0; } while(0)
+#define Hoogtesensor_SetOpenDrain()       do { ODCONCbits.ODCC2 = 1; } while(0)
+#define Hoogtesensor_SetAnalogMode()      do { ANSELCbits.ANSC2 = 1; } while(0)
+#define Hoogtesensor_SetDigitalMode()     do { ANSELCbits.ANSC2 = 0; } while(0)
+
+// get/set Hartslagsensor aliases
+#define Hartslagsensor_TRIS                 TRISCbits.TRISC4
+#define Hartslagsensor_LAT                  LATCbits.LATC4
+#define Hartslagsensor_PORT                 PORTCbits.RC4
+#define Hartslagsensor_WPU                  WPUCbits.WPUC4
+#define Hartslagsensor_OD                   ODCONCbits.ODCC4
+#define Hartslagsensor_ANS                  ANSELCbits.ANSC4
+#define Hartslagsensor_SetHigh()            do { LATCbits.LATC4 = 1; } while(0)
+#define Hartslagsensor_SetLow()             do { LATCbits.LATC4 = 0; } while(0)
+#define Hartslagsensor_Toggle()             do { LATCbits.LATC4 = ~LATCbits.LATC4; } while(0)
+#define Hartslagsensor_GetValue()           PORTCbits.RC4
+#define Hartslagsensor_SetDigitalInput()    do { TRISCbits.TRISC4 = 1; } while(0)
+#define Hartslagsensor_SetDigitalOutput()   do { TRISCbits.TRISC4 = 0; } while(0)
+#define Hartslagsensor_SetPullup()          do { WPUCbits.WPUC4 = 1; } while(0)
+#define Hartslagsensor_ResetPullup()        do { WPUCbits.WPUC4 = 0; } while(0)
+#define Hartslagsensor_SetPushPull()        do { ODCONCbits.ODCC4 = 0; } while(0)
+#define Hartslagsensor_SetOpenDrain()       do { ODCONCbits.ODCC4 = 1; } while(0)
+#define Hartslagsensor_SetAnalogMode()      do { ANSELCbits.ANSC4 = 1; } while(0)
+#define Hartslagsensor_SetDigitalMode()     do { ANSELCbits.ANSC4 = 0; } while(0)
+
+// get/set RC5 procedures
+#define RC5_SetHigh()            do { LATCbits.LATC5 = 1; } while(0)
+#define RC5_SetLow()             do { LATCbits.LATC5 = 0; } while(0)
+#define RC5_Toggle()             do { LATCbits.LATC5 = ~LATCbits.LATC5; } while(0)
+#define RC5_GetValue()              PORTCbits.RC5
+#define RC5_SetDigitalInput()    do { TRISCbits.TRISC5 = 1; } while(0)
+#define RC5_SetDigitalOutput()   do { TRISCbits.TRISC5 = 0; } while(0)
+#define RC5_SetPullup()             do { WPUCbits.WPUC5 = 1; } while(0)
+#define RC5_ResetPullup()           do { WPUCbits.WPUC5 = 0; } while(0)
+#define RC5_SetAnalogMode()         do { ANSELCbits.ANSC5 = 1; } while(0)
+#define RC5_SetDigitalMode()        do { ANSELCbits.ANSC5 = 0; } while(0)
+
+// get/set IO_RC7 aliases
+#define IO_RC7_TRIS                 TRISCbits.TRISC7
+#define IO_RC7_LAT                  LATCbits.LATC7
+#define IO_RC7_PORT                 PORTCbits.RC7
+#define IO_RC7_WPU                  WPUCbits.WPUC7
+#define IO_RC7_OD                   ODCONCbits.ODCC7
+#define IO_RC7_ANS                  ANSELCbits.ANSC7
+#define IO_RC7_SetHigh()            do { LATCbits.LATC7 = 1; } while(0)
+#define IO_RC7_SetLow()             do { LATCbits.LATC7 = 0; } while(0)
+#define IO_RC7_Toggle()             do { LATCbits.LATC7 = ~LATCbits.LATC7; } while(0)
+#define IO_RC7_GetValue()           PORTCbits.RC7
+#define IO_RC7_SetDigitalInput()    do { TRISCbits.TRISC7 = 1; } while(0)
+#define IO_RC7_SetDigitalOutput()   do { TRISCbits.TRISC7 = 0; } while(0)
+#define IO_RC7_SetPullup()          do { WPUCbits.WPUC7 = 1; } while(0)
+#define IO_RC7_ResetPullup()        do { WPUCbits.WPUC7 = 0; } while(0)
+#define IO_RC7_SetPushPull()        do { ODCONCbits.ODCC7 = 0; } while(0)
+#define IO_RC7_SetOpenDrain()       do { ODCONCbits.ODCC7 = 1; } while(0)
+#define IO_RC7_SetAnalogMode()      do { ANSELCbits.ANSC7 = 1; } while(0)
+#define IO_RC7_SetDigitalMode()     do { ANSELCbits.ANSC7 = 0; } while(0)
+
+/**
+   @Param
+    none
+   @Returns
+    none
+   @Description
+    GPIO and peripheral I/O initialization
+   @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+*/
